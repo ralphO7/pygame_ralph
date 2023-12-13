8bbabc24fdd5995a1e7f3e28aff127ab07c8f7a0
ralphO7 <139001857+ralphO7@users.noreply.github.com>
2023-12-13 17:52:35 +0100

    added score

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+from column import Column
 
 running = True
 # coordinates of the bird 
@@ -34,25 +34,49 @@ def move():
 
     if event.key == pygame.K_SPACE:
         bird_x += 10
-        bird_y += 2
+        bird_y -= 2
+
 
 pygame.init()
 
 pygame.display.set_caption('bro srxly')
 
+
 scrn = pygame.display.set_mode((500, 500))
 
+points = 0
+font = "JetBrainsMono-Medium.ttf"
+
 bird = pygame.Rect(bird_x, bird_y, 45, 45)
+column = pygame.Rect(random.randint(10, 489), 0, 5, 55)
+font1 = pygame.font.Font(font, 40)
+
 
 while True:
+    
+    #mouse pos 
+    mx, my = pygame.mouse.get_pos()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
         if event.type == pygame.KEYDOWN:
             move()
+    if bird.colliderect(column):
+        column.x = random.randint(10, 470)
+        column.y = random.randint(10, 470)
+        points += 1
+    
+    #color first
     scrn.fill('pink')
-    bird.y = bird_y
-    bird.x = bird_x     
-    pygame.draw.rect(scrn, 'red', bird)
+    bird.y = my
+    bird.x = mx     
+    points_count = font1.render(str(points), True, "white")
+    
     check_boundaries()
+    
+    scrn.blit(points_count, (240, 10))
+    pygame.draw.rect(scrn, 'red', column)
+    pygame.draw.rect(scrn, 'red', bird)
+    
     pygame.display.update()
