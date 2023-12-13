f95b72e9d388542345ee1fc83edd60d4b6dde669
ralphO7 <139001857+ralphO7@users.noreply.github.com>
2023-12-13 20:25:25 +0100

    final game 1

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 import pygame
 import random
+import math
 from column import Column
+import time
 
 running = True
 # coordinates of the bird 
 bird_x = 23 
 bird_y = 0 
-
+lives = 3
 velocity = random.randint(1, 10)
+colors = ['red ','purple', 'red', 'blue', 'black', 'gold']
+random_color = random.choice(colors)
 
 def check_boundaries():
-    global bird_y, bird_x
+    global bird_y, bird_x, points, lives
     
     if bird_y > 500 or bird_y < 10:
         bird_y = random.randint(1, 499)
+    if column.y > 500:
+        column.y = 0
+        column.x = random.randint(10, 470)
+        points -= 1
+        lives -= 1
     if bird_x > 500 or bird_x < 10:
         bird_x = random.randint(1, 499)
-    
-
+clock = pygame.time.Clock()
 def move():
     global bird_y, bird_x
     if event.key == pygame.K_DOWN:
@@ -39,7 +47,7 @@ def move():
 
 pygame.init()
 
-pygame.display.set_caption('bro srxly')
+pygame.display.set_caption('my GAME')
 
 
 scrn = pygame.display.set_mode((500, 500))
@@ -62,21 +70,32 @@ while True:
             quit()
         if event.type == pygame.KEYDOWN:
             move()
+    
+    column = column.move(0, 5)
+    clock.tick(100)
     if bird.colliderect(column):
         column.x = random.randint(10, 470)
-        column.y = random.randint(10, 470)
         points += 1
-    
+        column.y = 0
+        #column.y = random.randint(10, 470)
     #color first
     scrn.fill('pink')
+    
     bird.y = my
     bird.x = mx     
     points_count = font1.render(str(points), True, "white")
+    live = font1.render(str(lives), True, "black")
     
     check_boundaries()
     
+    if lives < 0 :
+        end_text = font1.render("You lose ", True, 'purple')
+        scrn.blit(end_text, (150, 150))
+        quit("bye")
+        
     scrn.blit(points_count, (240, 10))
-    pygame.draw.rect(scrn, 'red', column)
-    pygame.draw.rect(scrn, 'red', bird)
+    scrn.blit(live, (440, 10))
+    pygame.draw.rect(scrn, random_color, column)
+    pygame.draw.rect(scrn, random_color, bird)
     
     pygame.display.update()
